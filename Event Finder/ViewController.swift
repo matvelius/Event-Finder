937a16fd42fcc4e1cc431d39f7d85d8ba7b07c24
Matvey Kostukovsky <mkostukovsky@intouchconnections.com>
2021-01-23 18:45:49 -0600

    Initial Commit

---
+++ Event Finder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Event Finder
+//
+//  Created by Matvey Kostukovsky on 1/23/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
