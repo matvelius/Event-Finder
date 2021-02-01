c8891ac97ff8088495a3c7d4dc12b79e0149006b
Matvey Kostukovsky <mkostukovsky@intouchconnections.com>
2021-01-31 22:42:50 -0600

    remove unnecessary files

---
+++ Pods/Target Support Files/Pods-Event Finder-Event FinderUITests/Pods-Event Finder-Event FinderUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Event_Finder_Event_FinderUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Event_Finder_Event_FinderUITests
+@end
---
+++ Pods/Target Support Files/Pods-Event FinderTests/Pods-Event FinderTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Event_FinderTests : NSObject
+@end
+@implementation PodsDummy_Pods_Event_FinderTests
+@end
